da0c4178db601b3c639c5dcbeb06f2bec0db9601
David Toneian <david.toneian@accenture.com>
2021-08-01 23:45:59 +0200

    Allow positional arguments for `nox` to be passed to `pytest`.

--- template-project/noxfile.py
+++ template-project/noxfile.py
@@ -2,5 +2,6 @@ import nox
 
 @nox.session()
 def tests(session):
+    pytest_args = session.posargs or ["--cov"]
     session.run("poetry", "install", external=True)
-    session.run("pytest", "--cov")
+    session.run("pytest", *pytest_args)
