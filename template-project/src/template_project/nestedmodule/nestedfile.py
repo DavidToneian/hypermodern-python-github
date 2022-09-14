b529568f010120ee7ad6e92e8ef24394f49599f7
David Toneian <david@toneian.com>
2022-09-14 15:23:47 +0200

    Make example source code more diverse, add tests, fix flake8 complaint.

--- template-project/src/template_project/nestedmodule/nestedfile.py
+++ template-project/src/template_project/nestedmodule/nestedfile.py
@@ -1,8 +1,12 @@
-"""Documentation of nestedfile.py."""
+"""
+Documentation of nestedfile.py.
+"""
 
 
 def nestedfunc() -> None:
-    """No-op function."""
+    """
+    No-op function.
+    """
 
 
 class NestedClass:
@@ -10,7 +14,9 @@ class NestedClass:
     Class nested in :obj:`nestedmodule`, and in particular in
     :obj:`nestedmodule.nestedfile``, or :obj:`nestedfile` for short.
 
-    See the `Sphinx Documentation <https://www.sphinx-doc.org/en/master/usage/restructuredtext/domains.html#cross-referencing-python-objects>`_
+    See the `Sphinx Documentation \
+<https://www.sphinx-doc.org/en/master/usage/restructuredtext/domains.html\
+#cross-referencing-python-objects>`_
     for info how to cross-reference in general using Sphinx for
     Python.
     """
@@ -21,15 +27,23 @@ class NestedClass:
 
         :param x: Some integer that will be saved.
         """
+
         self._x = x
 
     def get_x(self) -> int:
-        """Return the saved integer."""
+        """
+        Return the saved integer.
+
+        :return: Returns the saved integer.
+        """
         return self._x
 
 
 class NestedChild(NestedClass):
     """Class deriving from :obj:`NestedClass`."""
+
+    def __init__(self) -> None:
+        super().__init__(123)
 
     @staticmethod
     def derived_static() -> None:
